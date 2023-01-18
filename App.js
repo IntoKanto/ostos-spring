0d5340828693bb6b1df24b0998ec6d893ae288f0
IntoKanto <anatoli.tselms@myy.haaga-helia.fi>
2023-01-18 16:52:44 +0200

    Toimii

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View, SafeAreaView, FlatList, Keyboard } from 'react-native';
 
 export default function App() {
+  const [item, setItem] = useState('');
+  const [list, setList] = useState('');
+
+  const add = ()=>{
+    setList([...list, {key: item}]);
+    setItem('');
+    Keyboard.dismiss()
+  }
+  const clear = () => {
+    setList([]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <TextInput style={styles.inputbox}
+      onChangeText={item => setItem(item)}
+      value={item}
+      />
+
+      <View style={styles.buttonfield}>
+        <Button title='ADD' onPress={add}/>
+        <Button title='CLEAR' onPress={clear}/>
+      </View>
+      <FlatList 
+      data={list}
+      renderItem={({item}) => <Text>{item.key}</Text>}
+      keyExtractor={(item, index) => index.toString()}
+      />
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +44,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputbox: {
+    width: 300,
+    borderWidth: 1,
+    borderColor: 'blue'
+  },
+  buttonfield: {
+    flexDirection: 'row',
+    alignItems: 'stretch'
+  }
 });
